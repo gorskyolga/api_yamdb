2aa335c47587760913f41636feefca06db09606d
Olga Avdeeva <gorskyolga@gmail.com>
2023-04-18 21:22:30 +0200

    Базовые настройки проекта и упрощенная реализация для эндпоинта titles

---
+++ api_yamdb/titles/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Title(models.Model):
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Название',
+    )
+    year = models.IntegerField(
+        verbose_name='Год выпуска',
+    )
+    description = models.TextField(
+        verbose_name='Описание',
+        blank=True,
+        null=True
+    )
